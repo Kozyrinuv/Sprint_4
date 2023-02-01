5450e32515f534897f3ffa6c990c23e1d2582320
Misha Kozyryn <hronorempi@mail.ru>
2023-02-01 11:04:24 +0300

    second edition sprint_4

---
+++ Sprint_4/src/test/java/ru/yandex/praktikum/scooter/pageobjects/ScooterHomePage.java
@@ -0,0 +1,136 @@
+package ru.yandex.praktikum.scooter.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+// Класс главной (начальной) страницы Самокат
+public class ScooterHomePage {
+    private final WebDriver driver;
+
+    private final By[] dropdownList;//Массив локаторов выпадающих списков "Вопросы о важном"
+    private final By[] dropdownListText;//Массив локаторов текстов выпадающих списков "Вопросы о важном"
+
+    //Локатор надписи "Самокат на пару дней" на главной странице
+    //$x(".//div[@class='Home_Header__iJKdX']")
+    private final By homePageTitle = By.className("Home_Header__iJKdX");
+
+    //Локатор кнопки "Заказать" в правом верхнем углу
+    //$x(".//div/button[@class='Button_Button__ra12g']")
+    private final By orderTopButton = By.className("Button_Button__ra12g");
+
+    //Локатор кнопки "Заказать" в центре снизу
+    //$x(".//div[@class='Home_FinishButton__1_cWm']/button")
+    private final By orderBottomButton = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button");
+
+    //Локатор кнопки "Статус заказа" в правом верхнем углу
+    //$x(".//div/button[@class='Header_Link__1TAG7']")
+    private final By orderStatusButton = By.className("Header_Link__1TAG7");
+
+    //Локаторы выпадающих списков "Вопросы о важном"
+    //$x(".//div[@id='accordion__heading-0']")
+    //$x(".//div[@id='accordion__heading-7']")
+    private final By dropdownList0 = By.id("accordion__heading-0");
+    private final By dropdownList1 = By.id("accordion__heading-1");
+    private final By dropdownList2 = By.id("accordion__heading-2");
+    private final By dropdownList3 = By.id("accordion__heading-3");
+    private final By dropdownList4 = By.id("accordion__heading-4");
+    private final By dropdownList5 = By.id("accordion__heading-5");
+    private final By dropdownList6 = By.id("accordion__heading-6");
+    private final By dropdownList7 = By.id("accordion__heading-7");
+
+    //Локаторы текстов выпадающих списков "Вопросы о важном"
+    //$x(".//div[@id='accordion__panel-0']/p")
+    //$x(".//div[@id='accordion__panel-7']/p")
+    private final By dropdownListText0 = By.id("accordion__panel-0");
+    private final By dropdownListText1 = By.id("accordion__panel-1");
+    private final By dropdownListText2 = By.id("accordion__panel-2");
+    private final By dropdownListText3 = By.id("accordion__panel-3");
+    private final By dropdownListText4 = By.id("accordion__panel-4");
+    private final By dropdownListText5 = By.id("accordion__panel-5");
+    private final By dropdownListText6 = By.id("accordion__panel-6");
+    private final By dropdownListText7 = By.id("accordion__panel-7");
+
+    //Конструктор класса ScooterHomePage
+    public ScooterHomePage(WebDriver driver) {
+        this.driver = driver;
+
+        //Массив локаторов выпадающих списков "Вопросы о важном"
+        dropdownList = new By[]{dropdownList0, dropdownList1, dropdownList2, dropdownList3, dropdownList4, dropdownList5, dropdownList6, dropdownList7};
+
+        //Массив локаторов текстов выпадающих списков "Вопросы о важном"
+        dropdownListText = new By[]{dropdownListText0, dropdownListText1, dropdownListText2, dropdownListText3, dropdownListText4, dropdownListText5, dropdownListText6, dropdownListText7};
+    }//end Конструктор ScooterHomePage
+
+    //------------------------------------------------------------
+    //Методы нажатия на стрелку выпадающего списка
+    private void clickDropdownList(By dropdownList) {
+        driver.findElement(dropdownList).click();
+    }
+
+    //-------------------------------------------------------------
+    //Методы получения текста ответа на вопрос выпадающего списка
+    private String getDropdownListText(By dropdownListText) {
+        return driver.findElement(dropdownListText).getText();
+    }
+
+    //-----------------------------------------------------------------
+    // метод  чтения текста i-го элемента выпадающего списка (считаем эл-ты с 0)
+    public String getListAnswer(int index) {
+        //Прокрутка до i-го элемента выпадающего списка
+        WebElement element = driver.findElement(dropdownList[index]);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+
+        //Ожидание доступности нажатия i-го элемента списка
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.elementToBeClickable(dropdownList[index]));
+
+        //Нажимаем на стрелку i-го элемента списка
+        clickDropdownList(dropdownList[index]);
+
+        //Ожидание видимости текста ответа
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(dropdownListText[index]));
+
+        //Возвращаем строку текста ответа на вопрос i-го элемента выпадающего списка
+        return getDropdownListText(dropdownListText[index]);
+
+    }//end public void checkListQuestion
+
+    //=================Методы нажатия кнопок "Заказать" для теста Flow==============================================
+    //Метод нажатия на кнопку "Заказать" в правом верхнем углу
+    public void clickOrderTop() {
+        driver.findElement(orderTopButton).click();
+    }
+
+    //----------------------------------------------------------------------------------------
+    //Метод нажатия на кнопку "Заказать" в центре страницы снизу
+    public void clickOrderBottom() {
+        //Прокрутка до кнопки "Заказать"
+        WebElement element = driver.findElement(orderBottomButton);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        timesleep(5);//Задержка выполнения теста
+
+        //Нажимаем на кнопку
+        driver.findElement(orderBottomButton).click();
+    }
+
+    //-------------------------------------------------------------------------------------
+    //Задержка для визуализации работы теста на экране
+    public void timesleep(int sec) {
+        try {
+            TimeUnit.SECONDS.sleep(sec);
+        }//Задержка в sec сек
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+//-------------------------------------------------------------------------------------------
+}//end class HomePageScooter
+
+
---
+++ Sprint_4/src/test/java/ru/yandex/praktikum/scooter/pageobjects/ScooterPersonPage.java
@@ -0,0 +1,125 @@
+package ru.yandex.praktikum.scooter.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+// Класс страницы "Для кого самокат"
+public class ScooterPersonPage {
+    private final WebDriver driver;
+
+    //Локатор поля ввода "Имя"
+    //$x(".//input[@placeholder='* Имя']")
+    private final By inputName = By.xpath(".//input[@placeholder='* Имя']");
+
+    //Локатор поля ввода "Фамилия"
+    //$x(".//input[@placeholder='* Фамилия']")
+    private final By inputSurname = By.xpath(".//input[@placeholder='* Фамилия']");
+
+    //Локатор поля ввода "Адрес: куда привезти заказ"
+    //$x(".//input[@placeholder='* Адрес: куда привезти заказ']")
+    private final By inputAddress = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
+
+    //Локатор поля выбора "Станция метро"
+    //$x(".//input[@placeholder='* Станция метро']")
+    private final By inputMetro = By.xpath(".//input[@placeholder='* Станция метро']");
+
+    //Локатор поля ввода "Телефон: на него позвонит курьер"
+    //$x(".//input[@placeholder='* Телефон: на него позвонит курьер']")
+    private final By inputTelephone = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
+
+    //Локатор кнопки "Далее"
+    //$x(".//div[@class = 'Order_NextButton__1_rCA']/button[@class='Button_Button__ra12g Button_Middle__1CSJM']")
+    private final By buttonNextToRent = By.xpath(".//div[@class = 'Order_NextButton__1_rCA']/button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+
+    //-------------------------------------------------------------------------
+    //Конструктор класса ScooterHomePage
+    public ScooterPersonPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //-----------------------------------------------------------------------------
+    //Метод ввода имени Заказчика самоката
+    private void setName(String name) {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(inputName));
+        driver.findElement(inputName).clear();
+        driver.findElement(inputName).sendKeys(name);
+    }
+
+    //---------------------------------------------------------------------------
+    //Метод ввода фамилии Заказчика самоката
+    private void setSurname(String Surname) {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(inputSurname));
+        driver.findElement(inputSurname).clear();
+        driver.findElement(inputSurname).sendKeys(Surname);
+    }
+
+    //------------------------------------------------------------------------
+    //Метод ввода адреса Заказчика самоката
+    private void setAddress(String Address) {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(inputAddress));
+        driver.findElement(inputAddress).clear();
+        driver.findElement(inputAddress).sendKeys(Address);
+    }
+
+    //-------------------------------------------------------------------------
+    //Метод ввода телефона Заказчика самоката
+    private void setTelephone(String Telephone) {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(inputTelephone));
+        driver.findElement(inputTelephone).clear();
+        driver.findElement(inputTelephone).sendKeys(Telephone);
+    }
+
+    //-------------------------------------------------------------------------
+    //Метод ввода станции метро Заказчика самоката
+    private void setMetro() {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.visibilityOfElementLocated(inputMetro));
+        driver.findElement(inputMetro).sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+    }
+
+    //--------------------------------------------------------------------------
+    //Метод нажатия кнопки "Далее" для перехода на страницу заполнения данных аренды
+    private void clickButtonNext() {
+        //Ожидание доступности кнопки "Далее" для перехода на страницу заполнения данных аренды
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.elementToBeClickable(buttonNextToRent));
+        // Нажимаем кнопку "Далее" для перехода на страницу заполнения данных аренды
+        driver.findElement(buttonNextToRent).click();
+    }
+
+    //--------------------------------------------------------------------------
+    //Заполнение данных Заказчика и переход на форму данных аренды самоката
+    public void setPerson(String name, String Surname, String Address, String Telephone) {
+        new WebDriverWait(driver, Duration.ofSeconds(100))
+                .until(ExpectedConditions.elementToBeClickable(buttonNextToRent));
+        setName(name);//Имя
+        setSurname(Surname);//Фамилия
+        setAddress(Address);//Адрес доставки
+        setMetro();//Станция метро
+        setTelephone(Telephone);//Телефон
+        timesleep(3);//Задержка выполнения теста
+        clickButtonNext();//Нажимаем кнопку "Далее" для перехода на страницу заполнения данных аренды
+    }
+
+    //------------------------------------------------------------------------------
+    //Задержка для визуализации работы теста на экране
+    public void timesleep(int sec) {
+        try {
+            TimeUnit.SECONDS.sleep(sec);
+        }//Задержка в sec сек
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}// end public class PersonPage
+
+
